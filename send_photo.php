3b3ce61f351c60fbd5f34c48b5171590be1d50c5
Alex <ahzeealex@gmail.com>
2023-07-07 09:17:38 +0300

    fix: remove enclosure

--- send_photo.php
+++ send_photo.php
@@ -23,51 +23,50 @@ if (isset($_SESSION['uploaded'])) {
     exit;
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
-        $file = $_FILES['file'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
+    $file = $_FILES['file'];
 
-        $allowedExtensions = ['jpg', 'png'];
-        $fileName = $file['name'];
-        $fileExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    $allowedExtensions = ['jpg', 'png'];
+    $fileName = $file['name'];
+    $fileExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
 
-        if (!in_array($fileExtension, $allowedExtensions)) {
-            echo "Invalid file type";
-            exit;
-        }
-
-        $maxFileSize = 2 * 1024 * 1024;
-        if ($file['size'] > $maxFileSize) {
-            echo "File too large";
-            exit;
-        }
-
-        $uploadDir = "images/";
-        $uploadPath = $uploadDir . $file['name'];
-
-        try {
-            if (move_uploaded_file($file['tmp_name'], $uploadPath)) {
-                if (!isset($_SESSION['uploadCount'])) {
-                    $_SESSION['uploadCount'] = 0;
-                };
-                $_SESSION['uploadCount']++;
-
-                $_SESSION['uploaded'] = true;
-
-                echo "Файл успешно загружен.";
-
-                header('Location: ' . $uploadPath);
-                exit;
-            } else {
-                echo "Ошибка при загрузке файла.";
-            }
-        } catch (Exception $e) {
-            echo "Ошибка при перемещении файла";
-            exit;
-        }
-    } else {
-        echo "Ошибка при загрузке файла.";
+    if (!in_array($fileExtension, $allowedExtensions)) {
+        echo "Invalid file type";
+        exit;
     }
+
+    $maxFileSize = 2 * 1024 * 1024;
+    if ($file['size'] > $maxFileSize) {
+        echo "File too large";
+        exit;
+    }
+
+    $uploadDir = "images/";
+    $uploadPath = $uploadDir . $file['name'];
+
+    try {
+        if (move_uploaded_file($file['tmp_name'], $uploadPath)) {
+            if (!isset($_SESSION['uploadCount'])) {
+                $_SESSION['uploadCount'] = 0;
+            };
+            $_SESSION['uploadCount']++;
+
+            $_SESSION['uploaded'] = true;
+
+            echo "Файл успешно загружен.";
+
+            header('Location: ' . $uploadPath);
+            exit;
+        } else {
+            echo "Ошибка при загрузке файла.";
+        }
+    } catch (Exception $e) {
+        echo "Ошибка при перемещении файла";
+        exit;
+    }
+} else {
+    echo "Ошибка при загрузке файла.";
 }
+
 
 ?>
